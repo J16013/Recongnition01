9814531dd2ee71441b781cf7cea98c5094c3896d
Loire8 <zero.t0907@gmail.com>
2018-05-24 12:57:03 +0900

    Recongnition01

--- src/recongnition01/Recongnition01_main.java
+++ src/recongnition01/Recongnition01_main.java
@@ -2,7 +2,10 @@ package recongnition01;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
@@ -13,7 +16,7 @@ public class Recongnition01_main {
 		// TODO 自動生成されたメソッド・スタブ
 
 		VisualRecognition service = new VisualRecognition("2018-03-19");
-		service.setApiKey("J16013");
+		service.setApiKey("c9492283a95fc45911de623c781297a751b85a00");
 
 		DetectFacesOptions detectFacesOptions = null;
 		try {
@@ -26,6 +29,37 @@ public class Recongnition01_main {
 		}
 		DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
 		System.out.println(result);
-	}
+		String s = String.valueOf(result);
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			JsonNode node = mapper.readTree(s);
+			int age_min=node.get("images").get(0).get("faces").get(0).get("age").get("min").asInt();
+			int age_max=node.get("images").get(0).get("faces").get(0).get("age").get("max").asInt();
+			double age_score=node.get("images").get(0).get("faces").get(0).get("age").get("score").asDouble();
+			String gender=node.get("images").get(0).get("faces").get(0).get("gender").get("gender").asText();
+			double gender_score=node.get("images").get(0).get("faces").get(0).get("gender").get("score").asDouble();
+			double face_location_width=node.get("images").get(0).get("faces").get(0).get("face_location").get("width").asDouble();
+			double face_location_height=node.get("images").get(0).get("faces").get(0).get("face_location").get("height").asDouble();
+			double face_location_left=node.get("images").get(0).get("faces").get(0).get("face_location").get("left").asDouble();
+			double face_location_top=node.get("images").get(0).get("faces").get(0).get("face_location").get("top").asDouble();
+			String image=node.get("images").get(0).get("image").asText();
+
+			System.out.println("age_min:"+age_min);
+			System.out.println("age_max:"+age_max);
+			System.out.println("age_score:"+age_score);
+			System.out.println("Gender:"+gender);
+			System.out.println("Gender_score:"+gender_score);
+			System.out.println("face_location_width:"+face_location_width);
+			System.out.println("face_location_height:"+face_location_height);
+			System.out.println("face_location_left:"+face_location_left);
+			System.out.println("face_location_top:"+face_location_top);
+			System.out.println("image:"+image);
+
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		}
 
 }
