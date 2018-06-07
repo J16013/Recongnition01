4717929b91177ad835ac0067594f8c42bcc5df08
Loire8 <zero.t0907@gmail.com>
2018-06-07 12:24:05 +0900

    fooooooooooooooooooooooooo!!!

--- src/recongnition01/Recongnition01_main.java
+++ src/recongnition01/Recongnition01_main.java
@@ -16,7 +16,7 @@ public class Recongnition01_main {
 		// TODO 自動生成されたメソッド・スタブ
 
 		VisualRecognition service = new VisualRecognition("2018-03-19");
-		service.setApiKey("J16013");
+		service.setApiKey("6291b66ede0d70249a3746fb7d55a31f66aa68ab");
 
 		DetectFacesOptions detectFacesOptions = null;
 		try {
@@ -33,27 +33,70 @@ public class Recongnition01_main {
 		ObjectMapper mapper = new ObjectMapper();
 		try {
 			JsonNode node = mapper.readTree(s);
-			int age_min=node.get("images").get(0).get("faces").get(0).get("age").get("min").asInt();
-			int age_max=node.get("images").get(0).get("faces").get(0).get("age").get("max").asInt();
-			double age_score=node.get("images").get(0).get("faces").get(0).get("age").get("score").asDouble();
-			String gender=node.get("images").get(0).get("faces").get(0).get("gender").get("gender").asText();
-			double gender_score=node.get("images").get(0).get("faces").get(0).get("gender").get("score").asDouble();
-			double face_location_width=node.get("images").get(0).get("faces").get(0).get("face_location").get("width").asDouble();
-			double face_location_height=node.get("images").get(0).get("faces").get(0).get("face_location").get("height").asDouble();
-			double face_location_left=node.get("images").get(0).get("faces").get(0).get("face_location").get("left").asDouble();
-			double face_location_top=node.get("images").get(0).get("faces").get(0).get("face_location").get("top").asDouble();
-			String image=node.get("images").get(0).get("image").asText();
+			int age_min=node.get("images")
+					.get(0).get("faces")
+					.get(0).get("age")
+					.get("min")
+					.asInt();
+			int age_max=node.get("images")
+					.get(0).get("faces")
+					.get(0).get("age")
+					.get("max")
+					.asInt();
+			double age_score=node.get("images")
+					.get(0).get("faces")
+					.get(0).get("age")
+					.get("score")
+					.asDouble();
+			int gender=node.get("images")
+					.get(0).get("faces")
+					.get(0).get("gender")
+					.get("gender")
+					.asInt();
+			double gender_score=node.get("images")
+					.get(0).get("faces")
+					.get(0).get("gender")
+					.get("score")
+					.asDouble();
+
+			double face_location_width=node.get("images")
+					.get(0).get("faces")
+					.get(0).get("face_location")
+					.get("width")
+					.asDouble();
+			double face_location_height=node.get("images")
+					.get(0).get("faces")
+					.get(0).get("face_location")
+					.get("height")
+					.asDouble();
+			double face_location_left=node.get("images")
+					.get(0).get("faces")
+					.get(0).get("face_location")
+					.get("left")
+					.asDouble();
+			double face_location_top=node.get("images")
+					.get(0).get("faces")
+					.get(0).get("face_location")
+					.get("top")
+					.asDouble();
+			String image=node.get("images")
+					.get(0).get("image")
+					.asText();
 
 			System.out.println("age_min:"+age_min);
 			System.out.println("age_max:"+age_max);
 			System.out.println("age_score:"+age_score);
 			System.out.println("Gender:"+gender);
 			System.out.println("Gender_score:"+gender_score);
+
 			System.out.println("face_location_width:"+face_location_width);
 			System.out.println("face_location_height:"+face_location_height);
 			System.out.println("face_location_left:"+face_location_left);
 			System.out.println("face_location_top:"+face_location_top);
 			System.out.println("image:"+image);
+
+			MySQL mysql = new MySQL();
+			mysql.updateImage(age_min, age_max, age_score, gender, gender_score);
 
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
